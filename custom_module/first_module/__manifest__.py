51ab8244e2553295cbf1b10d6aaa77ab0b22db9d
Sherif213 <shouldtheone@gmail.com>
2025-02-05 17:03:51 +0300

    Feat : Adding Practicing models

---
+++ custom_module/first_module/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'First Module',
+    'author': 'Serif',
+    'version': '17.0.0.1.0',
+    'category': 'Training',
+    'depends': ['base'],
+    'data': [],
+    'application' : True,
+
+}
