8dfe742fc5613d77e17028a5683f9d4ae0221464
Sherif213 <shouldtheone@gmail.com>
2025-02-11 00:10:30 +0300

    Feat : adding SQL constraints and python constraints

--- custom_module/first_module/models/model_a.py
+++ custom_module/first_module/models/model_a.py
@@ -2,4 +2,4 @@ from odoo import models
 
 class ModelA(models.Model):
     _name = 'model.a'
-
+    _description = 'Model A'
--- custom_module/grading_management/models/grade.py
+++ custom_module/grading_management/models/grade.py
@@ -1,4 +1,6 @@
-from odoo import models , fields
+from odoo import models , fields ,api
+from odoo.exceptions import ValidationError
+
 
 class Grade(models.Model):
     _name = 'grade'
@@ -33,5 +35,83 @@ class Grade(models.Model):
     loyality_allowance_senior = fields.Float(string='Loyalty Allowance Senior Entry', required=True, default = 0.0)
     loyality_allowance_specialist = fields.Float(string='Loyalty Allowance Specialist', required=True, default = 0.0)
     loyality_allowance_manager = fields.Float(string='Loyalty Allowance Manager', required=True, default = 0.0)
+
+    _sql_constraints = [
+        ('grade_uniqueness','unique("grade_number")','Grade number must be unique!'),
+    ]
+
+    @api.constrains('medical_allowance','internet_allowance','insurance')
+    def _greater_than_zero_must_allowance(self):
+        for record in self:
+            if record.medical_allowance <= 0 or record.internet_allowance <= 0 or record.insurance <= 0:
+                raise ValidationError( "The following allowances must be greater than zero:\n"
+                "- Medical Allowance\n"
+                "- Internet Allowance\n"
+                "- Insurance\n"
+                )
+
+    @api.constrains('female_allowance')
+    def _greater_or_equal_zero_allowance_by_gender(self):
+        for record in self:
+            if record.female_allowance < 0 :
+                raise ValidationError( "The following allowances must be greater than zero:\n"
+                                       "- Female Allowance\n")
+
+    # @api.constrains('sales_bonus_entry_rate','sales_bonus_senior_rate'
+    #     ,'sales_bonus_specialist_rate','sales_bonus_manager_rate','travel_allowance','food_allowance','device_allowance',
+    #                 'loyality_allowance_Entry','loyality_allowance_senior','loyality_allowance_specialist',)
+    # # def _greater_or_equal_zero_additional_allowance(self):
+    #     for record in self:
+    #         if any(allowance < 0 for allowance in [
+    #             record.sales_bonus_entry_rate,record.sales_bonus_senior_rate,record.sales_bonus_specialist_rate,
+    #             record.sales_bonus_manager_rate,record.travel_allowance,record.food_allowance,
+    #             record.device_allowance,record.loyality_allowance_Entry,record.loyality_allowance_senior,
+    #             record.loyality_allowance_specialist,]):
+    #             raise ValidationError( "The following allowances must be greater than or equal to zero:\n"
+    #                                    "- Sales Bonus Entry\n"
+    #                                    "- Sales Bonus Senior Entry\n"
+    #                                    "- Sales Bonus Specialist\n"
+    #                                    "- Sales Bonus Manager\n"
+    #                                    "- Travel Allowance\n"
+    #                                    "- Food Allowance\n"
+    #                                    "- Device Allowance\n"
+    #                                    "- Loyalty allowance Entry\n"
+    #                                    "- Loyalty allowance senior\n"
+    #                                    "- Loyalty allowance specialist\n")
+
+
+    @api.constrains('sales_bonus_entry_rate', 'sales_bonus_senior_rate'
+                , 'sales_bonus_specialist_rate', 'sales_bonus_manager_rate')
+    def _greater_or_equal_zero_sales_allowance(self):
+        for record in self:
+            if any(allowance < 0 or allowance > 100 for allowance in [record.sales_bonus_entry_rate,record.sales_bonus_senior_rate,record.sales_bonus_specialist_rate,record.sales_bonus_manager_rate,]):
+                        raise ValidationError("The following allowances must be greater than or equal to zero:\n"
+                                              "- Sales Bonus Entry\n"
+                                              "- Sales Bonus Senior Entry\n"
+                                              "- Sales Bonus Specialist\n"
+                                              "- Sales Bonus Manager\n"
+                                              )
+
+    @api.constrains('travel_allowance', 'food_allowance', 'device_allowance')
+    def _greater_or_equal_zero_remote_allowance(self):
+        for record in self:
+            if any(allowance < 0 for allowance in [record.travel_allowance,record.food_allowance,record.device_allowance,]):
+                raise ValidationError("The following allowances must be greater than or equal to zero:\n"
+                                                "- Travel Allowance\n"
+                                                "- Food Allowance\n"
+                                                "- Device Allowance\n"
+                                                  )
+
+    @api.constrains('loyality_allowance_Entry', 'loyality_allowance_senior', 'loyality_allowance_specialist')
+    def _greater_or_equal_zero_loyal_allowance(self):
+        for record in self:
+            if any(allowance < 0 for allowance in [record.loyality_allowance_Entry,record.loyality_allowance_senior,record.loyality_allowance_specialist,]):
+                raise ValidationError("The following allowances must be greater than or equal to zero:\n"
+                                        "- Loyalty allowance Entry\n"
+                                        "- Loyalty allowance senior\n"
+                                        "- Loyalty allowance specialist\n"
+                                                  )
+
+
 
     
